5facd940c008e3f4a965b88bbe67d601b32a3c06
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-10-18 21:14:52 +0100

    challenge code complete

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,6 +1,10 @@
-﻿string permission = "Admin|Manager";
+﻿string permission = "Manager|Admin";
 int level = 55;
 
 if (permission.Contains("Admin")) {
   Console.WriteLine($"Welcome, {(level > 55 ? "Super " : "")}Admin user.");
+} else if (permission.Contains("Manager") && level >= 20) {
+  Console.WriteLine("Contact an Admin for access.");
+} else {
+  Console.WriteLine("You do not have sufficient privileges.");
 }
